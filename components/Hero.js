1d532596172a1783810aad062c04c3ac8a559caa
Andrea <andreabrunoxy@gmail.com>
2022-04-21 12:40:37 +0200

    add hero background

--- components/Hero.js
+++ components/Hero.js
@@ -2,18 +2,20 @@ import { Fade } from "react-awesome-reveal";
 
 const Hero = () => {
   return (
-    <div className="max-w-3xl mx-auto px-4 sm:px-6 lg:px-0 flex flex-col lg:items-center py-20 dark:bg-gray-800">
-      <Fade cascade triggerOnce>
-        <h1 className="text-6xl text-gray-900 lg:text-center font-bold max-w-lg dark:text-gray-100">
-          My Amazing{" "}
-          <Fade triggerOnce>
-            <span className="text-blue-900 dark:text-blue-500">Website</span>
-          </Fade>
-        </h1>
-        <p className="text-gray-700 font-semibold text-xl mt-8 lg:text-center dark:text-gray-100">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-        </p>
-      </Fade>
+    <div className="h-full py-32 bg-gradient-to-r from-blue-200 via-blue-300 to-blue-800 w-full mb-12 flex flex-col justify-center">
+      <div className="max-w-3xl mx-auto px-4 sm:px-6 lg:px-0 flex flex-col py-20">
+        <Fade cascade triggerOnce>
+          <h1 className="text-6xl text-gray-900 lg:text-center font-bold max-w-lg ">
+            My Amazing{" "}
+            <Fade triggerOnce>
+              <span className="text-blue-900 dark:text-blue-500">Website</span>
+            </Fade>
+          </h1>
+          <p className="text-gray-100 font-semibold text-xl mt-8 lg:text-center">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+          </p>
+        </Fade>
+      </div>
     </div>
   );
 };
